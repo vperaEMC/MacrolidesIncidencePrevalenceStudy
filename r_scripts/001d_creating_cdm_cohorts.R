9c955159c35d27b7c920f987ab0321603fa27d4d
vpera <v.pera@erasmusmc.nl>
2024-10-08 11:48:28 +0200

    renamed cubsets -> cohorts

---
+++ r_scripts/001d_creating_cdm_cohorts.R
@@ -0,0 +1,25 @@
+library(here)
+library(CDMConnector)
+library(dplyr)
+
+################################################################################
+######################## CDM, subset ###########################################
+################################################################################
+
+# The way with CDMconnector to create cohorts within CDM object using json files
+path_to_cohort_json_files <- path_setting_cohort
+list.files(path_to_cohort_json_files) 
+
+# read the json files that signify cohorts
+# cohort definition ids are like 1, 2, 3 etc, but the cohort "name"  (i.e.
+# atlas id number) is written next to it.
+cohort_set <- CDMConnector::read_cohort_set(path_to_cohort_json_files)
+
+
+# get the cohorts into MAIN CDM
+cdm <- CDMConnector::generate_cohort_set(
+  cdm = cdm,
+  cohort_set = cohort_set,
+  name = "stud_cohorts",
+  overwrite = TRUE
+)
